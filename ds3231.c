e5ae0599fd5b5157fc686e370a5f504ec10b8e3b
saiompolu <saiompolu@gmail.com>
2020-10-23 21:44:11 -0700

    second change

--- ds3231.c
+++ ds3231.c
@@ -15,24 +15,47 @@
 #include<linux/watchdog.h>
 
 
+int i2c_master_send(struct i2c_client *client, const char *buf,int count);
+
+int i2c_master_recv(struct i2c_client *client, char *buf,int count);
+
+struct ds3231{
+	struct device *dev;
+	struct regmap *regmap;
+	const char *name;
+	struct rtc_device *rtc;
+};
+
 
 static int ds3231_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
 	
 }
-static struct i2c_driver ds3231_driver={
-	.driver={
-		.name="rtc-ds3231".
-		
-
-	},
-	.probe	=ds3231_probe,
-	.id_table=ds3231_id,
-};
+static int ds3231_remove(struct i2c_client *client);
+{
 
 }
 
+static const struct i2c_device_id ds3231_idtable[]={
+	{"ds3231",0},
+	{}
+};
+MODULE_DEVICE_TABLE(i2c,ds3231_idtable);
+static struct i2c_driver ds3231_driver={
+	.driver={
+		.name="rtc-ds3231",
+		
+		
+
+	},
+	.id_table=ds3231_idtable,
+	.probe	=ds3231_probe,
+	.remove= ds3231_probe,
+	
+};
+
 module_i2c_driver(ds3231_driver);
+
 MODULE_AUTHOR("Ompolu Sai Naga Pothachari <saiompolu@gmail.com>");
 MODULE_DESCRIPTION("DS3231 RTC Driver");
 MODULE_LICENSE("GPL");
